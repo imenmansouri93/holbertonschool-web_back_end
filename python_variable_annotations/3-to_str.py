6142ce567017e04ebb752e0b73845be06fb19873
imenmansouri93 <imennmansourii@gmail.com>
2023-10-01 18:59:03 +0200

    new

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Converts a floating-point number to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_string function
+    """
+    return str(n)
