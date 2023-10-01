052504d7ade61a9d79702a6fd2abe9ff2a5806ab
imenmansouri93 <imennmansourii@gmail.com>
2023-10-01 19:03:36 +0200

    new

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+ Computes the sum of a list of floating-point numbers.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    sum_list function
+    """
+    return sum(input_list)
