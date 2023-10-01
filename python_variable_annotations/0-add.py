f62c29de5d9da0de68ad3508da516e309c02a4eb
imenmansouri93 <imennmansourii@gmail.com>
2023-10-01 18:49:50 +0200

    new

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-#!/usr/bin/env python3
+add that takes a float a and a float
+b as arguments and returns their sum as a float.
 """
+
+
 def add(a: float, b: float) -> float:
-
-
-
     """
     add function
     """
