03e676dcaba3324b1cce44aa1648395a3a5f3d1b
imenmansouri93 <imennmansourii@gmail.com>
2023-10-01 18:47:28 +0200

    new

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """
+#!/usr/bin/env python3
 """
 def add(a: float, b: float) -> float:
 
-    
-    """
-    Adds two floats and returns their sum as a float.
 
-    :param a: The first float.
-    :param b: The second float.
-    :return: The sum of a and b as a float.
+
+    """
+    add function
     """
     return a + b
