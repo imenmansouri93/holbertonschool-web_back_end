79644e0805701fc623c62ddef1536d6495056385
imenmansouri93 <imennmansourii@gmail.com>
2023-10-01 18:56:14 +0200

    new

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Returns the floor of a floating-point number as an integer.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    floor function
+    """
+    return math.floor(n)
