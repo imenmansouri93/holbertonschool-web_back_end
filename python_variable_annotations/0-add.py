1bcef44de026f9a0d31cf0c5bf321ad9df8cbf56
imenmansouri93 <imennmansourii@gmail.com>
2023-10-01 18:41:49 +0200

    new

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats and returns their sum as a float.
+
+    :param a: The first float.
+    :param b: The second float.
+    :return: The sum of a and b as a float.
+    """
+    return a + b
