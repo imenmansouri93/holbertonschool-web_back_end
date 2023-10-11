94448d99ad95cfa8427a168e10fc6fc5566e20dc
imenmansouri93 <imennmansourii@gmail.com>
2023-10-11 16:57:06 +0200

    new

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -28,7 +28,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         Save a new user to the database
