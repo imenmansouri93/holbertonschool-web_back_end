cfc1b6e4c45ef42f7bee2a4acaefc51963be459c
imenmansouri93 <imennmansourii@gmail.com>
2023-10-14 19:00:05 +0200

    new

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -9,6 +9,7 @@ AUTH = Auth()
 def welcome():
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
     """register user"""
@@ -19,7 +20,7 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
-    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
