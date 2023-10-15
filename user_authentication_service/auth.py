dab910db6066c54ef86a2369b3d50de117987154
imenmansouri93 <imennmansourii@gmail.com>
2023-10-15 21:22:04 +0200

    new

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,3 +85,12 @@ class Auth():
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        pwd = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
