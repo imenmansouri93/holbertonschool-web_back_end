be90c749138f20bb5ee73e19a3cb3e0fa92dfd43
imenmansouri93 <imennmansourii@gmail.com>
2023-10-15 21:10:41 +0200

    new

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -77,7 +77,7 @@ class Auth():
             return None
 
     def get_reset_password_token(self, email: str) -> str:
-        """if exist, generate a UUID and update the user's reset_token database field. return token"""
+        """if exist, generate a UUID and update the user's"""
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
@@ -85,5 +85,4 @@ class Auth():
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
-
 
