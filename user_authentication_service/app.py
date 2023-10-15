3e9a9da8db0c2e95bb2e83c5bfd22ffdcae5ee9d
imenmansouri93 <imennmansourii@gmail.com>
2023-10-15 20:38:50 +0200

    new

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
+import crypt
+from db import DB
 app = Flask(__name__)
 AUTH = Auth()
-
+db = DB()
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome():
@@ -20,6 +22,18 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Log in"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password) is False:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
 
 
 if __name__ == "__main__":
