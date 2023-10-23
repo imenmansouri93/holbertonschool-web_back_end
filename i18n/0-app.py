3996cc791de390ab1652e27fc36259cc026e4e1a
imenmansouri93 <imennmansourii@gmail.com>
2023-10-23 17:48:50 +0200

    new

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+"""Flask app
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def root():
+    """render to index.html"""
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
