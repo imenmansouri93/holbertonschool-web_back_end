db80a856ced508d1c83329d7dc0fb45e3a7305e2
imenmansouri93 <imennmansourii@gmail.com>
2023-10-23 18:11:39 +0200

    new

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -17,16 +17,20 @@ class Config:
 
 
 app.config.from_object(Config)
-
+"""
+"""
 
 @app.route('/')
 def index():
     """render to index.html"""
     return render_template('2-index.html')
 
+
 @babel.localeselector
 def get_locale():
+    """determine the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
