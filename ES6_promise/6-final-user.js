709e42891dd9ad4f622dab56513407c6ce16914e
imenmansouri93 <imennmansourii@gmail.com>
2023-11-17 16:50:25 +0100

    new

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,30 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {
+    status: 'pending',
+  };
+  const promise2 = {
+    status: 'pending',
+  };
+  try {
+    const upload = uploadPhoto(fileName);
+    promise1.status = 'fulfilled';
+    promise1.value = upload;
+  } catch (error) {
+    promise1.status = 'rejected';
+    promise1.value = error.toString();
+  }
+  try {
+    const signup = signUpUser(firstName, lastName);
+    promise2.status = 'fulfilled';
+    promise2.value = signup;
+  } catch (error) {
+    promise1.status = 'rejected';
+    promise1.value = error.toString();
+  }
+  return [promise1, promise2];
+}
+
+export default handleProfileSignup;
