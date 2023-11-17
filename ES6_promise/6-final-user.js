3d6eccb51c2ad3ad08bf0640949b7d5a9fbbb414
imenmansouri93 <imennmansourii@gmail.com>
2023-11-17 16:54:31 +0100

    new

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-async function handleProfileSignup(firstName, lastName, fileName) {
+function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = {
     status: 'pending',
   };
   const promise2 = {
     status: 'pending',
   };
+
   try {
-    const upload = await uploadPhoto(fileName);
+    const upload = uploadPhoto(fileName);
     promise1.status = 'fulfilled';
     promise1.value = upload;
   } catch (error) {
@@ -17,14 +18,15 @@ async function handleProfileSignup(firstName, lastName, fileName) {
     promise1.value = error.toString();
   }
   try {
-    const signup = await signUpUser(firstName, lastName);
+    const signup = signUpUser(firstName, lastName);
     promise2.status = 'fulfilled';
     promise2.value = signup;
   } catch (error) {
     promise1.status = 'rejected';
     promise1.value = error.toString();
   }
-  return [promise1, promise2];
+
+  return [promise2, promise1];
 }
 
 export default handleProfileSignup;
