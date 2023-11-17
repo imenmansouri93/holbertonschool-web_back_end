b55e29babdd0c6079296706dbcffefa170ad787b
imenmansouri93 <imennmansourii@gmail.com>
2023-11-17 14:32:58 +0100

    new

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,5 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an asynchronous API call (you would replace this with your actual API call)
-    setTimeout(() => {
-      if (true) {
-        resolve();
-      } else {
-        reject(); // Reject with an error
-      }
-    });
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve();
   });
 }
