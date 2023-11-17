51b87f4278c3301b5b65045f0f1b131f0f636d26
imenmansouri93 <imennmansourii@gmail.com>
2023-11-17 16:05:38 +0100

    new

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,9 +4,9 @@ function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const { body } = values[0];
-      const { firsName } = values[1];
+      const { firstName } = values[1];
       const { lastName } = values[1];
-      console.log(`${body} ${firsName} ${lastName}`);
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
