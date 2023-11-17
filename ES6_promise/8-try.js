5497566b570aad7ae71dff6fc6be0b85b2567273
imenmansouri93 <imennmansourii@gmail.com>
2023-11-17 17:22:14 +0100

    new

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,10 +1,6 @@
 function divideFunction(numerator, denominator) {
-  if (denominator !== 0) {
-    try {
-      return (numerator / denominator);
-    } catch (Error) {
-      throw Error('cannot divide by 0');
-    }
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
   }
   return (numerator / denominator);
 }
